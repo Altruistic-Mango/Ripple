029855de4df575ead7305ecd080674e17fb1cdf0
Natalie Spencer <nisca88@gmail.com>
2015-05-25 13:02:19 -0700

    made it so user position is set and sent on login and watch on position is set

---
+++ shout/www/app/login/login.factory.js
@@ -0,0 +1,17 @@
+angular
+  .module('shout.login')
+  .factory('LoginFactory', LoginFactory);
+
+LoginFactory.$inject = ['LocationFactory'];
+
+function LoginFactory(LocationFactory) {
+  var successfulLogin = function() {
+    console.log('login factory successfulLogin called')
+    LocationFactory.getCurrentPosition(LocationFactory.getSuccessCallback, LocationFactory.errorCallback);
+    LocationFactory.setWatch(LocationFactory.watchSuccessCallback, LocationFactory.errorCallback)
+  }
+  
+  return {
+    successfulLogin: successfulLogin
+  }
+}
