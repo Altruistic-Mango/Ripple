6f206e539c4737445e587d62dc3bf69df748505b
Matt Brown <mattfdbrown@gmail.com>
2015-05-25 10:37:38 -0700

    add camera services

---
+++ shout/www/app/camera/camera.services.js
@@ -0,0 +1,64 @@
+angular
+  .module('shout.camera')
+  .factory('CameraFactory', CameraFactory);
+
+CameraFactory.$inject = ['$rootScope'];
+
+function CameraFactory($rootScope) {
+  console.log('CameraFactory');
+
+  var uploadurl = "http://localhost/upl";
+  var pictureSource;
+  var destinationType; // sets the format of returned value
+  var picture;
+  initialize();
+
+  services = {};
+  services.query = query;
+  services.takePicture = takePicture;
+
+  return services;
+
+  function initialize() {
+    ionic.Platform.ready(function() {
+      if (!navigator.camera) {
+        // error handling
+        console.log('no camera found');
+        return;
+      }
+      //pictureSource=navigator.camera.PictureSourceType.PHOTOLIBRARY;
+      pictureSource = navigator.camera.PictureSourceType.CAMERA;
+      destinationType = navigator.camera.DestinationType.FILE_URI;
+    });
+  }
+
+  function query() {
+    console.log('query');
+    return uploadurl;
+  }
+
+  function takePicture() {
+    console.log('takePicture');
+    var options = {
+      quality: 50,
+      destinationType: destinationType,
+      sourceType: pictureSource,
+      encodingType: 0
+    };
+    if (!navigator.camera) {
+      // error handling
+      console.log('no camera found');
+      return;
+    }
+    navigator.camera.getPicture(
+      function(imageURI) {
+        console.log("got camera success ", imageURI);
+        mypicture = imageURI;
+      },
+      function(err) {
+        // error handling camera plugin
+        console.log("got camera error ", err);
+      },
+      options);
+  }
+}
