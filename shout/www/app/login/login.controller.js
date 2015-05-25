6d7dcc6aedc3e7e08395a9cdb2844b4360f626fb
Natalie Spencer <nisca88@gmail.com>
2015-05-25 13:02:19 -0700

    made it so user position is set and sent on login and watch on position is set

--- shout/www/app/login/login.controller.js
+++ shout/www/app/login/login.controller.js
@@ -2,11 +2,12 @@ angular
   .module('shout.login')
   .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['$scope', '$state', 'LocationFactory'];
+LoginCtrl.$inject = ['$scope', '$state','LoginFactory'];
 
-function LoginCtrl($scope, $state, LocationFactory) {
+function LoginCtrl($scope, $state, LoginFactory) {
   console.log('LoginCtrl');
   $scope.login = function() {
+    LoginFactory.successfulLogin(); 
     $state.go('tab.inbox');
   };
 }
