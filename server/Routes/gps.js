3b2fb00a7da46e4abf2ae8b8d2d5aaad61c5aa4d
henrymollman <hmollman@gmail.com>
2015-05-25 12:11:17 -0700

    Added quadtree functionality to test and urls

--- server/Routes/gps.js
+++ server/Routes/gps.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var gpsController = require('../Controllers/gpsController.js')
 
-router.post('/newdata', function(req, res) {
+router.post('/position', function(req, res) {
   console.log('adding new GPS data');
   gpsController.insertCoords(req, res);
 });
