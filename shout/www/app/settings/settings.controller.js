f332629469d4635459aa9ad714a396e31e3eaae6
Natalie Spencer <nisca88@gmail.com>
2015-05-25 18:00:45 -0700

    create UI for settings view with toggle and sliders

--- shout/www/app/settings/settings.controller.js
+++ shout/www/app/settings/settings.controller.js
@@ -9,24 +9,23 @@ function SettingsCtrl($state, $ionicHistory) {
 
   var vm = this;
 
-  vm.radius = 0; //TODO: determine an appropriate initial value
-  vm.TTL = 0; //TODO: determine an appropriate initial value
+  vm.radius = 5; //TODO: determine an appropriate initial value
+  vm.TTL = 5; //TODO: determine an appropriate initial value
   vm.watch = true; 
   vm.acceptSettings = acceptSettings;
-  vm.userSetWatch = userSetWatch;
 
   function acceptSettings() {
     //do something with the settings, probably involving sending the information
     //to the settings factory
     //then go back to the last view
+    
     if ($ionicHistory.backView()) {
       $state.go('tab.' + $ionicHistory.backTitle().toLowerCase());
     } else {
       $state.go('tab.inbox');
     }
+    console.log('radius set to: ', parseInt(vm.radius));
+    console.log('TTL set to: ', parseInt(vm.TTL))
   }
 
-  function userSetWatch() {
-    console.log('value of toggle: ', vm.watch);
-  }
 }
