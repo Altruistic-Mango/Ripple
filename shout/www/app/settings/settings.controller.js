e5aa77a19cc2c22b9213bcd67aa043b4d534b7c3
Natalie Spencer <nisca88@gmail.com>
2015-05-25 19:34:03 -0700

    implement toggle watch location

--- shout/www/app/settings/settings.controller.js
+++ shout/www/app/settings/settings.controller.js
@@ -2,30 +2,32 @@ angular
   .module('shout.settings')
   .controller('SettingsCtrl', SettingsCtrl);
 
-SettingsCtrl.$inject = ['$state', '$ionicHistory'];
+SettingsCtrl.$inject = ['$state', '$ionicHistory', 'SettingsFactory'];
 
-function SettingsCtrl($state, $ionicHistory) {
+function SettingsCtrl($state, $ionicHistory, SettingsFactory) {
   console.log('SettingsCtrl');
 
   var vm = this;
 
-  vm.radius = 5; //TODO: determine an appropriate initial value
-  vm.TTL = 5; //TODO: determine an appropriate initial value
+  vm.radius = 5; //initial value 5 miles
+  vm.TTL = 5; //initial value 5 minutes
   vm.watch = true; 
   vm.acceptSettings = acceptSettings;
+  vm.userSetWatch = userSetWatch; 
 
   function acceptSettings() {
-    //do something with the settings, probably involving sending the information
-    //to the settings factory
-    //then go back to the last view
-    
+    SettingsFactory.setSettings(parseInt(vm.radius), parseInt(vm.TTL)); 
     if ($ionicHistory.backView()) {
-      $state.go('tab.' + $ionicHistory.backTitle().toLowerCase());
+      $ionicHistory.goBack();
     } else {
       $state.go('tab.inbox');
     }
     console.log('radius set to: ', parseInt(vm.radius));
     console.log('TTL set to: ', parseInt(vm.TTL))
+  }
+
+  function userSetWatch() {
+    SettingsFactory.setWatch(vm.watch);
   }
 
 }
