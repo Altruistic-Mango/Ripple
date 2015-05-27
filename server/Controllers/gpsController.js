d9212140a58e15ea9cf9088aad973d355af0225f
henrymollman <hmollman@gmail.com>
2015-05-26 19:47:14 -0700

    Updated event controller

--- server/Controllers/gpsController.js
+++ server/Controllers/gpsController.js
@@ -3,6 +3,8 @@ var quadtree = require('../Utils/QTree.js');
 
 var gpsController = {
 
+
+  // this will insert a coordinate to the quadtree for insertion
   insertCoords: function(req, res) {
 
     var node = {
@@ -15,7 +17,9 @@ var gpsController = {
     res.send('Entered node');
   },
 
+  // this function takes a request from the user and returns an array of nodes that are within the quadrant
   findNearbyNodes: function(req, res) {
+    console.log(req.body);
     
     var searchParams = {
       x: req.body.x, 
@@ -23,10 +27,55 @@ var gpsController = {
       username: req.body.username
     };
 
+    var nearbyNodes = this.getNodes(searchParams);
+    res.send(nearbyNodes);
+  },
+
+  // This will get the distance between two coordinates
+  calculateDist: function(item1) {
+
+    var nodes = this.getNodes(item1);
+    item1.x = Math.abs(item1.x);
+    nodes.forEach(function(item2) {
+    var hyp = Math.abs((item1.x + item2.x) * (item1.x + item2.x)) + 
+              Math.abs((item1.y - item2.y) * (item1.y - item2.y));
+    hyp = Math.sqrt(hyp);    
+    });
+
+    return hyp;
+  },
+
+  // Find nodes in Quadtree
+  getNodes: function(searchParams) {
     var nodes = quadtree.get(searchParams);
-    res.send(nodes);
-  }
+    return nodes;
+  },
+
+  // Invoke calculate distance function
+  getDist: function(req, res) {
+    var result = this.calculateDist(req.body)
+    res.send(result)
+  },
+
+  // remove nodes from quadtree if they match the item sent
+  removeNode: function(item) {
+    return quadtree.remove(item);
+  },
+
+  // initiate the removal function
+  initRemove: function(req, res) {
+    var item = this.removeNode(req.body);
+    res.send(item);
+  },
+
+  // load dummy data for testing
+  loadData: function(req, res) {
+    quadtree.addData();
+  },
+
 };
 
+
+>>>>>>> Added removal and distance calculation for quadtree
 
 module.exports = gpsController;
