38e782ffef2e9f1842f94236be996ae1a041476b
Natalie Spencer <nisca88@gmail.com>
2015-05-27 14:22:43 -0700

    implement simple user sign up and login; not real authentication

--- shout/www/app/login/login.factory.js
+++ shout/www/app/login/login.factory.js
@@ -2,18 +2,28 @@ angular
   .module('shout.login')
   .factory('LoginFactory', LoginFactory);
 
-LoginFactory.$inject = ['LocationFactory'];
+LoginFactory.$inject = ['LocationFactory', 'InboxFactory', '$localstorage', '$http'];
 
-function LoginFactory(LocationFactory) {
+function LoginFactory(LocationFactory, InboxFactory, $localstorage, $http) {
+  return {
+    successfulLogin: successfulLogin,
+    loginUser: loginUser
+  }
   
+  function loginUser (data) {
+    return $http({
+      method: 'POST',
+      url: '/users/signin',
+      data: data
+    })
+  }
 
-  var successfulLogin = function() {
+  function successfulLogin (data) {
     console.log('login factory successfulLogin called')
+    $localstorage.set('userId', data.userId);
+    InboxFactory.updateInbox(data.inbox)
     LocationFactory.getCurrentPosition(LocationFactory.getSuccessCallback, LocationFactory.errorCallback);
     LocationFactory.setWatch(LocationFactory.watchSuccessCallback, LocationFactory.errorCallback)
   }
   
-  return {
-    successfulLogin: successfulLogin
-  }
 }
--- shout/www/app/login/signup.controller.js
+++ shout/www/app/login/signup.controller.js
@@ -2,15 +2,24 @@ angular
   .module('shout.signup')
   .controller('SignupCtrl', SignupCtrl);
 
-SignupCtrl.$inject = ['$state'];
+SignupCtrl.$inject = ['$state', 'SignupFactory'];
 
-function SignupCtrl($state) {
+function SignupCtrl($state, SignupFactory) {
   console.log('SignupCtrl');
   var vm = this;
+  vm.data; 
 
-  vm.login = login;
+  vm.signup = signup;
   
-  function login() {
-    $state.go('tab.inbox');
+  function signup() {
+    console.log('vm.data: ', vm.data);
+    SignupFactory.signupUser(vm.data)
+                 .then(function (res) {
+                    console.log('response from server on singup: ', res);
+                    if (res.status === 200){
+                      $state.go('login');
+                    }
+                  });
   }
+  
 }
