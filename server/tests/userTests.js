5d4060d857766affabc5808416a6f87373557f2f
Eden Mazzola <mazzolaeden@gmail.com>
2015-05-27 14:26:00 -0700

    Started testing for signing policy doc

--- server/tests/userTests.js
+++ server/tests/userTests.js
@@ -54,35 +54,4 @@ describe('user controller', function(){
 
 
 
-/* testing the docSign function and encrytion
-make a new user
-fake http post with appropriate data
-should respond with object containing appropriate properties(key, etc...)
-fake an http post with inapropiate data*/
-
-// describe('photos', function(){
-
-// 	it('Test should return a 200 status code', function(done){
-// 		supertest(app)
-// 		.get('/photos/test')
-// 		.expect(200)
-// 		.end(function(err, res){
-// 			res.status.should.equal(200);
-// 			done();
-// 		});
-// 	});
- 
-//   it('should successfully store a photo to the database', function(done){
-
-//   	supertest(app)
-//   	.post('/photos/newPhoto')
-//   	.set('Content-Type', 'application/json')
-//   	.send('{ "photoId": "1234", "radius": "12", "TTL": "12", "photoURL": "www.neat.com", "recipientList": "[]"}')
-//   	.end(function(err, res){
-//   		res.status.should.equal(200);
-//   		done();
-//   	});
-//   });
-// });
-
 
