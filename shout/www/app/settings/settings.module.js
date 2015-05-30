7a12f99bb704d24c3890aed8a3835f20aa2ad5d1
Matt Brown <mattfdbrown@gmail.com>
2015-05-29 17:44:52 -0700

    update dependencies

--- shout/www/app/settings/settings.module.js
+++ shout/www/app/settings/settings.module.js
@@ -1,5 +1,9 @@
 angular.module('shout.settings', [
-  's3UploadApp'
+  's3UploadApp',
+  'shout.camera',
+  'shout.location',
+  's3UploadApp',
+  'shout.constants'
   // 'shout.login'
   //list the other modules that contain factories and controllers that you will use
 ]);
--- shout/www/dist/shout.js
+++ shout/www/dist/shout.js
@@ -61,7 +61,11 @@ angular.module('s3UploadApp', [
 ]);
 
 angular.module('shout.settings', [
-  's3UploadApp'
+  's3UploadApp',
+  'shout.camera',
+  'shout.location',
+  's3UploadApp',
+  'shout.constants'
   // 'shout.login'
   //list the other modules that contain factories and controllers that you will use
 ]);
