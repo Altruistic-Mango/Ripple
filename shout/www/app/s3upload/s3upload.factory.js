56d74c1a24be0e5e8293b2a3b4a8903645d70402
Matt Brown <mattfdbrown@gmail.com>
2015-06-01 14:25:32 -0700

    refactor, decoupled and modularized, reduce dependencies
    a lot of work went into this, much more modular now

--- shout/www/app/s3upload/s3upload.factory.js
+++ shout/www/app/s3upload/s3upload.factory.js
@@ -1,85 +1,75 @@
 angular
-  .module('s3UploadApp')
-  .factory('s3Upload', s3Upload);
+  .module('s3Upload')
+  .factory('s3', s3);
 
-s3Upload.$inject = ['$http', '$location', '$upload', '$rootScope', '$localstorage', 'API_HOST'];
+s3.$inject = ['$http', 'API_HOST'];
 
-function s3Upload($http, $location, $upload, $rootScope, $localstorage, API_HOST) {
+function s3($http, API_HOST) {
 
-  var imageUploads = [];
-  var upload = [];
+  var userId = 'userId';
+  var url = 'https://' + 'ripple-photos' + '.s3.amazonaws.com/';
+  var file = new File(); 
+
   var services = {};
-  var files = [];
-
-  services.uploadFile = uploadFile;
+  services.upload = upload;
+  services.userId = userId;
 
   return services;
 
-  function abort(index) {
-    upload[index].abort();
-    upload[index] = null;
+  function upload(newfile, callback) {
+    file = newfile;
+    console.log('uploadToS3');
+
+    getSignedPolicy(function(response) {
+      sendFile(response, function(){
+        callback();
+      });
+    });
   }
 
-  $scope.uploadFile = uploadFile;
-
-  function uploadFile($files) {
-    $files = [$files];
-    files = $files;
-    console.log(files);
-    upload = [];
-    for (var i = 0; i < $files.length; i++) {
-      var file = $files[i];
-      file.progress = parseInt(0);
-      (function(file, i) {
-        $http.get(API_HOST + '/api/s3Policy?mimeType=' + file.type).success(function(response) {
-          var s3Params = response;
-          $localstorage.set('timestamp', Date.now());
-          var photoId = $localstorage.get('userId') + $localstorage.get('timestamp');
-          upload[i] = $upload.upload({
-            url: 'https://' + $rootScope.config.awsConfig.bucket + '.s3.amazonaws.com/',
-            method: 'POST',
-            transformRequest: function(data, headersGetter) {
-              //Headers change here
-              var headers = headersGetter();
-              delete headers['Authorization'];
-              return data;
-            },
-            data: {
-              //'key': 's3Upload/' + Math.round(Math.random() * 10000) + '$$' + file.name,
-              'key': 's3Upload/' + photoId + '.jpeg',
-              'acl': 'public-read',
-              'Content-Type': file.type,
-              'AWSAccessKeyId': s3Params.AWSAccessKeyId,
-              'success_action_status': '201',
-              'Policy': s3Params.s3Policy,
-              'Signature': s3Params.s3Signature
-            },
-            file: file,
-          });
-          console.log(file);
-          upload[i]
-            .then(function(response) {
-              file.progress = parseInt(100);
-              if (response.status === 201) {
-                var data = xml2json.parser(response.data),
-                  parsedData;
-                parsedData = {
-                  location: data.postresponse.location,
-                  bucket: data.postresponse.bucket,
-                  key: data.postresponse.key,
-                  etag: data.postresponse.etag
-                };
-                imageUploads.push(parsedData);
-
-              } else {
-                alert('Upload Failed');
-              }
-            }, null, function(evt) {
-              file.progress = parseInt(100.0 * evt.loaded / evt.total);
-            });
-        });
-      }(file, i));
-    }
-    console.log('done with upload for loop');
+  function getSignedPolicy(callback) {
+    console.log('getSignedPolicy');
+    $http.get(API_HOST + '/api/s3Policy?mimeType=' + file.type)
+      .success(function(response) {
+        callback(response);
+      });
   }
+
+  function sendFile(s3Params, callback) {
+    console.log('sendFile');
+    var extension = file.type.match(/\w+$/)[0];
+    var params = {
+      'key': 's3Upload/' + file.name + '.' + extension,
+      'acl': 'public-read',
+      'Content-Type': file.type,
+      'AWSAccessKeyId': s3Params.AWSAccessKeyId,
+      'success_action_status': '201',
+      'Policy': s3Params.s3Policy,
+      'Signature': s3Params.s3Signature
+     };
+
+    var options = new FileUploadOptions();
+    options.fileKey = 'file';
+    options.fileName = file.name + '.' + extension;
+    options.mimeType = file.type;
+    options.chunkedMode = false;
+    options.params = params;
+    console.log(options);
+    var ft = new FileTransfer();
+    ft.upload(file.localURL, url, callback, fail, options);
+  }
+
+  // Helper Functions
+  function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+  }
+
+  function fail(error) {
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
+    console.log(error);
+  }
+
 }
