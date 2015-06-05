3c369ce647169e99517e4858461f79a3255c5a2c
Matt Brown <mattfdbrown@gmail.com>
2015-06-04 19:48:55 -0700

    add TODOs in settings, cleanup shout.run

--- shout/www/app/shout.run.js
+++ shout/www/app/shout.run.js
@@ -2,25 +2,28 @@ angular
   .module('shout')
   .run(run);
 
-run.$inject = ['$http', '$rootScope', 'API_HOST'];
+run.$inject = ['$http', '$localstorage', '$rootScope', 'API_HOST'];
 
-function run($http, $rootScope, API_HOST) {
-  console.log('shout run');
+function run($http, $localstorage, $rootScope, API_HOST) {
   ionic.Platform.ready(function() {
-    console.log('ionic platform ready');
+    var user = $localstorage.get('user');
 
-    $http.get(API_HOST+'/api/config').success(function(config) {
-      $rootScope.config = config;
-    });
+    //TODO: in login section, create this variable
+    //if (user.isSignedIn) {
+    //  $state.go('tab.inbox');
+    //} else {
+    //  $state.go('login');
+   // }
 
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    //hide keyboard accessory
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
+    // org.apache.cordova.statusbar required
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+
   });
 }
