f569b82723e82c00d51df66f87f4449b14f25d18
henrymollman <hmollman@gmail.com>
2015-06-13 09:41:01 -0700

    Increased queue removal interval

--- server/Utils/Queue.js
+++ server/Utils/Queue.js
@@ -9,7 +9,7 @@ function Queue() {
 
 	obj.removeFromQueue = function() {
 		var timestamp = new Date().getTime();
-		if (_queue.length && timestamp - _queue[0].timestamp > 60000) {
+		if (_queue.length && timestamp - _queue[0].timestamp > 90000) {
       console.log('Current time in ms = ' + timestamp + ' object timestamp = ' + _queue[0].timestamp + ' = ' + (timestamp - _queue[0].timestamp))
       console.log('Removing user ' + JSON.stringify(_queue[0]));
 			return _queue.shift();
